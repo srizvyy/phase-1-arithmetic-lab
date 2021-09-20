5acbf655bd6b6095d55b074510f7e9d751586a11
Saim Rizvi <shr2mz@gmail.com>
2021-09-20 19:53:41 -0400

    commit tree b66d5564cc19bb45ef2b461a66bc2b494e19597a

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 1
+const num2 = 62
 
-// Write your code here
+const multiply = num1 * num2
+
+const random = 100
+Math.random()
+
+let num3 = 4
+let num4 = 16
+
+const mod = num3 % num4
+
+const max = (1, 2, 3, 5, 6, 20)
+math.max()
